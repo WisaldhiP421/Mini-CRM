978e3d16e87c20119d56db33728e37e9d7fe8f5a
WisaldhiP421 <putrawisaldhi@gmail.com>
2022-11-25 09:29:13 +0700

    change text action to icon and add link

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Company;
 use App\Models\Employee;
-use App\Models\Checkbox;
 
 
 class EmployeeController extends Controller
